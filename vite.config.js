d6bfa49e7e1c62d7e412730e8ed27dd1653fdebe
Shivam Sutar <shivamsutar33@hotmail.com>
2025-10-30 00:59:48 +0530

    fix: Rearrange server.js build configuration and update routes in vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
