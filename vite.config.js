8fe4b51b9551eee1bbdbb0d0b7563abd2c8c741a
Shivam Sutar <shivamsutar33@hotmail.com>
2025-10-30 12:40:34 +0530

    config fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  // const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react(), tailwindcss()],
     // base: "./",
@@ -13,15 +13,15 @@ export default defineConfig(({ mode }) => {
     //   assetsDir: "assets",
     //   sourcemap: true,
     // },
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL, // 👈 backend server
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, "/api"),
-        },
-      },
-    },
+    // server: {
+    //   proxy: {
+    //     "/api": {
+    //       target: env.VITE_API_URL, // 👈 backend server
+    //       changeOrigin: true,
+    //       secure: false,
+    //       rewrite: (path) => path.replace(/^\/api/, "/api"),
+    //     },
+    //   },
+    // },
   };
 });
